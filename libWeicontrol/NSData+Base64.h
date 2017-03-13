a6195d926aa3e1b558e7efdacd0dead8cc9f3ce7
yaoshenglin <yaoshenglin@qq.com>
2017-03-13 15:07:15 +0800

    @function功能失效

--- libWeicontrol/NSData+Base64.h
+++ libWeicontrol/NSData+Base64.h
@@ -34,8 +34,7 @@
 
 @interface NSData (Base64)
 
-/*!	
- @function	+dataWithBase64EncodedString:
+/*!
  @discussion	This method returns an autoreleased NSData object. The NSData object is initialized with the
  contents of the Base 64 encoded string. This is a convenience method.
  @param	string	An NSString object that contains only Base 64 encoded data.
@@ -43,8 +42,7 @@
  */
 + (NSData *) dataWithBase64EncodedString:(NSString *) string;
 
-/*!	
- @function	-initWithBase64EncodedString:
+/*!
  @discussion	The NSData object is initialized with the contents of the Base 64 encoded string.
  This method returns self as a convenience.
  @param	string	An NSString object that contains only Base 64 encoded data.
@@ -53,7 +51,6 @@
 - (id) initWithBase64EncodedString:(NSString *) string;
 
 /*!	
- @function	-base64EncodingWithLineLength:
  @discussion	This method returns a Base 64 encoded string representation of the data object.
  @param	lineLength A value of zero means no line breaks.  This is crunched to a multiple of 4 (the next
  one greater than inLineLength).
